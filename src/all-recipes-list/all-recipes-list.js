4b04b02834c5e91c522df29017b7a57d983e3f62
Paulina Siwko <pau.siwko@gmail.com>
2023-05-12 18:12:58 +0200

    added all recipes page

--- src/all-recipes-list/all-recipes-list.js
+++ src/all-recipes-list/all-recipes-list.js
@@ -1,10 +1,36 @@
 import React from "react";
 import './all-recipes-list.css';
 import MainMenu from "../components/main-menu";
-
+import {Container, Row} from "react-bootstrap";
+import RecipeCard from "../recipe-card/recipe-card";
+import RecommendedCarousel from "../components/recommended-carousel";
 export default function AllRecipesList() {
     return (
-        <MainMenu />
+        <div>
+            <MainMenu />
+            <h3 className='allRecipesTitle'>Ideas for breakfast</h3>
+            <Container fluid>
+                <Row>
+                    <RecipeCard cardTitle={'Avocado toast'}
+                                cardImage={'avocado-toast.jpg'}/>
+                    <RecipeCard cardTitle={'English breakfast'}
+                                cardImage={'english-breakfast.jpg'}/>
+                    <RecipeCard cardTitle={'Omlette'}
+                                cardImage={'omlette.PNG'}/>
+                    <RecipeCard cardTitle={'Waffle'}
+                                cardImage={'waffle.jpg'}/>
+                    <RecipeCard cardTitle={'English breakfast'}
+                                cardImage={'english-breakfast.jpg'}/>
+                    <RecipeCard cardTitle={'Omlette'}
+                                cardImage={'omlette.PNG'}/>
+                    <RecipeCard cardTitle={'Waffle'}
+                                cardImage={'waffle.jpg'}/>
+                    <RecipeCard cardTitle={'Avocado toast'}
+                                cardImage={'avocado-toast.jpg'}/>
+                </Row>
+            </Container>
+            <RecommendedCarousel />
+        </div>
     );
 }
 
---
+++ src/recipe-card/recipe-card.js
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+import './recipe-card.css';
+import {Card} from "react-bootstrap";
+import {FaRegHeart} from "react-icons/fa";
+import {FcLike} from "react-icons/fc";
+import {Link} from "react-router-dom";
+export default function RecipeCard({ cardTitle, cardImage }) {
+    const [isLiked, setIsLiked] = useState(true);
+
+    function handleHeart() {
+        setIsLiked(!isLiked);
+    }
+
+    return (
+            <Card className='recipeCard'>
+                <Card.Img variant='top'
+                          className='cardPhoto'
+                          src={require(`../img/breakfast/${cardImage}`)} />
+                <Card.Body>
+                    <Card.Title>
+                        {isLiked ? (
+                            <FaRegHeart size='22'
+                                        onClick={handleHeart} />
+                        ) : (
+                            <FcLike size='22'
+                                    onClick={handleHeart} />
+                        )}
+                        {' '}
+                        <Link to={'/single-recipe-page'}
+                              className='cardLink'>
+                            {cardTitle}
+                        </Link>
+                    </Card.Title>
+                </Card.Body>
+            </Card>
+    );
+}
+
